a3ff08f3717ed2186f15aebd158a4e6cbb76846a
Lilith <bichakhchyanlilith1992@gmail.com>
2026-03-31 13:41:27 +0300

    template has added

--- mailing/views.py
+++ mailing/views.py
@@ -5,6 +5,7 @@ from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import UpdateView
 
+from mailing.forms import RecipientForm
 from mailing.models import Recipient
 
 
@@ -14,22 +15,22 @@ class RecipientList(ListView):
     context_object_name = "recipients"
 
 
-class RecipientDetailView(DetailView):
-    model = Recipient
-
-
-class RecipientCreateView(CreateView):
-    model = Recipient
-    success_url = reverse_lazy("mailing:recipients_list")
-
-
-class RecipientUpdateView(UpdateView):
-    model = Recipient
-    form_class = RecipientForm
-    success_url = reverse_lazy("mailing:recipients_list")
-
-
-
-class RecipientDeleteView(DeleteView):
-    model = Recipient
-    success_url = reverse_lazy("mailing:recipients_list")
+# class RecipientDetailView(DetailView):
+#     model = Recipient
+#
+#
+# class RecipientCreateView(CreateView):
+#     model = Recipient
+#     success_url = reverse_lazy("mailing:recipients_list")
+#
+#
+# class RecipientUpdateView(UpdateView):
+#     model = Recipient
+#     form_class = RecipientForm
+#     success_url = reverse_lazy("mailing:recipients_list")
+#
+#
+#
+# class RecipientDeleteView(DeleteView):
+#     model = Recipient
+#     success_url = reverse_lazy("mailing:recipients_list")
