799aee45fc772f311ca7a0f96f7cc31ae305f314
Lilith <bichakhchyanlilith1992@gmail.com>
2026-04-22 01:36:48 +0300

    Set client cache on detailView pages

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from mailing.apps import MailingConfig
 from mailing.views import DisableMailing
@@ -26,21 +27,21 @@ app_name = MailingConfig.name
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("recipient/", RecipientList.as_view(), name="recipient_list"),
-    path("recipient_detail/<int:pk>/", RecipientDetailView.as_view(), name="recipient_detail"),
+    path("recipient_detail/<int:pk>/",  cache_page(60 * 15)(RecipientDetailView.as_view()), name="recipient_detail"),
     path("recipient/new/", RecipientCreateView.as_view(), name="recipient_create"),
     path("recipient/update/<int:pk>/", RecipientUpdateView.as_view(), name="recipient_update"),
     path("recipient/delete/<int:pk>/", RecipientDeleteView.as_view(), name="recipient_delete"),
     path("message/", MessageList.as_view(), name="message_list"),
-    path("message_detail/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("message_detail/<int:pk>/",  cache_page(60 * 15)(MessageDetailView.as_view()), name="message_detail"),
     path("message/new/", MessageCreateView.as_view(), name="message_create"),
     path("message/update/<int:pk>/", MessageUpdateView.as_view(), name="message_update"),
     path("message/delete/<int:pk>/", MessageDeleteView.as_view(), name="message_delete"),
     path("'mailing/", MailingList.as_view(), name="mailing_list"),
-    path("mailing_detail/<int:pk>/", MailingDetailView.as_view(), name="mailing_detail"),
+    path("mailing_detail/<int:pk>/",  cache_page(60 * 15)(MailingDetailView.as_view()), name="mailing_detail"),
     path("mailing/new/", MailingCreateView.as_view(), name="mailing_create"),
     path("mailing/update/<int:pk>/", MailingUpdateView.as_view(), name="mailing_update"),
     path("mailing/delete/<int:pk>/", MailingDeleteView.as_view(), name="mailing_delete"),
-    path("send_mailig/<int:pk>/", SendMailingDetailView.as_view(), name="send_mailing"),
+    path("send_mailig/<int:pk>/",  cache_page(60 * 15)(SendMailingDetailView.as_view()), name="send_mailing"),
     path("mailing_attempt/", MailingAttemptList.as_view(), name="mailing_attempt_list"),
     path("disable_mailing/<int:pk>/", DisableMailing.as_view(), name="disable_mailing"),
 ]
